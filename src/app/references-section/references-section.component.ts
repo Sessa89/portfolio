e685544cbd8b0d8e13ecc22a7de8b9cf6ffed879
Philip Baumgärtner <philip_baumgaertner@hotmail.de>
2025-01-21 20:48:23 +0100

    created components

---
+++ src/app/references-section/references-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-references-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './references-section.component.html',
+  styleUrl: './references-section.component.scss'
+})
+export class ReferencesSectionComponent {
+
+}
