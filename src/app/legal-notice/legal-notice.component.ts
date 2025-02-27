abc104cb2659d4a309aca04e70468e321243a201
Philip Baumgärtner <philip_baumgaertner@hotmail.de>
2025-02-27 05:39:41 +0100

    added legal-notice and privacy-policy and updated ContactMeComponent and FooterComponent

---
+++ src/app/legal-notice/legal-notice.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../shared/header/header.component';
+
+@Component({
+  selector: 'app-legal-notice',
+  standalone: true,
+  imports: [HeaderComponent],
+  templateUrl: './legal-notice.component.html',
+  styleUrl: './legal-notice.component.scss'
+})
+export class LegalNoticeComponent {
+
+}
