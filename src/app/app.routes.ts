6b76820778053598ffa140948803a96f53445c10
Philip Baumgärtner <philip_baumgaertner@hotmail.de>
2025-02-28 12:43:17 +0100

    created supplementary content for projectdetails

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
+import { ProjectDetailsComponent } from './shared/project-details/project-details.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
 
 export const routes: Routes = [
     { path: '', component: MainContentComponent },
+    { path: 'project-details', component: ProjectDetailsComponent },
     { path: 'privacy-policy', component: PrivacyPolicyComponent },
     { path: 'legal-notice', component: LegalNoticeComponent },
 ];
--- src/app/shared/project-details/project-details.component.ts
+++ src/app/shared/project-details/project-details.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '../header/header.component';
+import { AnimatedSubheadingComponent } from '../animated-subheading/animated-subheading.component';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'app-project-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeaderComponent, AnimatedSubheadingComponent, ButtonComponent],
   templateUrl: './project-details.component.html',
   styleUrl: './project-details.component.scss'
 })
